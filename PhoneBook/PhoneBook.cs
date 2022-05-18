f97bc53b51fba40e2f1ceb276b0b2a5adbfaae89
ewnnr <90215819+ewnnr@users.noreply.github.com>
2022-05-18 10:47:06 +0200

    Add files via upload

---
+++ PhoneBook/PhoneBook.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PhoneBook
+{
+    class PhoneBook
+    {
+        private List<Contact> _contacts { get; set; } = new List<Contact>();
+        
+        //Metod för att visa kontakter
+        private void DisplayContacts(Contact contact)
+        {
+            Console.WriteLine($"Kontakt: {contact.Name}, {contact.Number}");
+        }
+        //Metod för att visa loopa igenom alla kontakter som finns i listan, och sedan visa dem med hjälp av tidigare metod. 
+        private void ContactList(List<Contact> contacts)
+        {
+            foreach (var contact in contacts)
+            {
+                DisplayContacts(contact);
+            }
+        }
+
+        //Metod för att lägga till kontakt i kontaktboken
+        public void AddContact(Contact contact)
+        {
+            _contacts.Add(contact);
+        }
+        //Metod för att visa en kontakt med nummer
+        public void DisplayContact (string number)
+        {
+            var contact = _contacts.FirstOrDefault(c => c.Number == number);
+            if (contact == null)
+            {
+                Console.WriteLine("Kontakten hittades inte");
+            }
+            else
+            //Här används tidigare skapad metod
+            {
+                DisplayContacts(contact); 
+            }
+        }
+        //Metod för att visa alla kontakter
+        public void DisplayAll()
+        {
+            ContactList(_contacts);
+        }
+
+        //Metod för att söka efter kontakt med namn
+        public void SearchContact(string searchPhrase)
+        {
+            var matchingContacts = _contacts.Where(c => c.Name.Contains(searchPhrase)).ToList();
+            ContactList(matchingContacts);
+        }
+    }
+}
---
+++ PhoneBook/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace PhoneBook
+{
+    internal class Program
+    {
+        //Menyn med de olika valen för telefonboken. 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("TELEFONBOK");
+            Console.WriteLine("Välj alternativ nedan");
+            Console.WriteLine("1. Lägg till kontakt");
+            Console.WriteLine("2. Sök kontakt med telefonnummer");
+            Console.WriteLine("3. Visa alla kontakter");
+            Console.WriteLine("4. Sök kontakt med namn");
+            Console.WriteLine("Tryck x för att stänga telefonboken");
+            //Sparar användarens val i en variabel
+            var userInput = Console.ReadLine();
+            //Telefonbokens klass sparad i en variabel
+            var phoneBook = new PhoneBook();
+            //Whileloop med true för att köra loopen medans alternativ finns
+            while (true)
+            {//Switch-sats för de olika alternativen, här används användarens tidigare inmatade val, för att komma på rätt alternativ. 
+                switch (userInput)
+                {
+                    //Lägger till ny kontakt med hjälp av metoden AddContact som skapades i klassen PhoneBook
+                    case "1":
+                        Console.WriteLine("Namn på kontakten:");
+                        var name = Console.ReadLine();
+                        Console.WriteLine("Kontaktens nummer:");
+                        var number = Console.ReadLine();
+
+                        var newContact = new Contact(name, number);
+                        phoneBook.AddContact(newContact);
+
+                        break;
+
+                        //Sök efter nummer i kontaktboken, med hjälp av metoden DisplayContact från klassen PhoneBook
+                    case "2":
+                        Console.WriteLine("Nummer att söka efter:");
+                        var searchNumber = Console.ReadLine();
+                        phoneBook.DisplayContact(searchNumber);
+
+                        break;
+
+                        //Visa alla kontakter, med metden DisplayAll från klassen PhoneBook
+                    case "3":
+                        phoneBook.DisplayAll();
+
+                        break;
+
+                        //Sök efter kontakt med namn, metoden SearchContact från klassen Phonebook
+                    case "4":
+                        Console.WriteLine("Skriv namn att söka efter:");
+                        var searchName = Console.ReadLine();
+                        phoneBook.SearchContact(searchName);
+
+                        break;
+
+                        //Alternativ för att stänga programmet. 
+                    case "x":
+                        return;
+
+                        //Default om användaren anger felaktigt alternativ i menyn. 
+                    default:
+                        Console.WriteLine("Ange giltigt alternativ!");
+                        break;
+                }
+                //Whileloopen gör så att användaren kan välja nytt alternativ från menyn när tidigare valt alternativ har utförts. 
+                Console.WriteLine("Välj nytt alternativ:");
+                userInput = Console.ReadLine();
+            }
+
+        }
+
+            
+    }
+}
